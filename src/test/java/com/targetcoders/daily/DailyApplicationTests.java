b61e628420312dbddd314eb7b4b18c0b9df57aa2
greenneuron <goodev@kakao.com>
2022-09-28 20:31:41 +0900

    create project

---
+++ src/test/java/com/targetcoders/daily/DailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.targetcoders.daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
