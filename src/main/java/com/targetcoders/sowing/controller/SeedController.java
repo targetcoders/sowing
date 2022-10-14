556db2eb9d5ac7dc427387f43e8822d7635a9d2c
greenneuron <goodev@kakao.com>
2022-10-15 02:52:30 +0900

    시드 조회 화면 추가
    메인 화면에서 보이는 시드 목록에는 시드 타입과 제목만 표시합니다. 조회하고 싶은 시드를 클릭하면 내용을 확인할 수 있습니다.

--- src/main/java/com/targetcoders/sowing/controller/SeedController.java
+++ src/main/java/com/targetcoders/sowing/controller/SeedController.java
@@ -1,5 +1,6 @@
 package com.targetcoders.sowing.controller;
 
+import com.targetcoders.sowing.seed.Seed;
 import com.targetcoders.sowing.seed.SeedForm;
 import com.targetcoders.sowing.seed.SeedService;
 import com.targetcoders.sowing.seed.SeedType;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
@@ -23,13 +25,20 @@ public class SeedController {
     public String seedForm(Model model) {
         List<SeedType> typeList = Arrays.stream(SeedType.values()).collect(Collectors.toList());
         model.addAttribute("typeList", typeList);
-        return "createSeedForm";
+        return "seeds/createSeedForm";
     }
 
     @PostMapping("/seeds/new")
     public String create(SeedForm seedForm) {
         seedService.saveSeed(seedForm);
         return "redirect:/";
+    }
+
+    @GetMapping("/seeds/{id}")
+    public String seedFind(Model model, @PathVariable("id") Long id) {
+        Seed findSeed = seedService.findSeedById(id);
+        model.addAttribute("seed", findSeed);
+        return "seeds/seed";
     }
 
 }
