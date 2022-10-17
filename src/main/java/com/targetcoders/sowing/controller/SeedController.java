44b95a48cf5a381b29ddae345e87c6c6194caf7f
greenneuron <goodev@kakao.com>
2022-10-17 23:41:49 +0900

    시드 수정 기능 추가
    시드 조회 페이지에서 수정 가능하도록 기능을 추가합니다. 삭제 기능은 버튼만 추가합니다.

--- src/main/java/com/targetcoders/sowing/controller/SeedController.java
+++ src/main/java/com/targetcoders/sowing/controller/SeedController.java
@@ -1,16 +1,12 @@
 package com.targetcoders.sowing.controller;
 
-import com.targetcoders.sowing.seed.Seed;
-import com.targetcoders.sowing.seed.SeedForm;
-import com.targetcoders.sowing.seed.SeedService;
-import com.targetcoders.sowing.seed.SeedType;
+import com.targetcoders.sowing.seed.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,6 +35,33 @@ public class SeedController {
         Seed findSeed = seedService.findSeedById(id);
         model.addAttribute("seed", findSeed);
         return "seeds/seed";
+    }
+
+    @GetMapping("seeds/{id}/edit")
+    public String update(Model model, @PathVariable("id") Long id) {
+        Seed seed = seedService.findSeedById(id);
+        SeedForm seedForm = new SeedForm();
+        seedForm.setTitle(seed.getTitle());
+        seedForm.setSelectType(seed.getType());
+        seedForm.setContent(seed.getContent());
+        seedForm.setUsername(seed.getMember().getUsername());
+        model.addAttribute("seed", seedForm);
+        List<SeedType> typeList = Arrays.stream(SeedType.values()).collect(Collectors.toList());
+        seedForm.setTypeList(typeList);
+        seedForm.setId(id);
+        return "seeds/editSeedForm";
+    }
+
+    @PostMapping("seeds/{id}/edit")
+    public String update(@ModelAttribute("form") SeedForm seedForm) {
+        UpdateSeedDTO updateSeedDTO = new UpdateSeedDTO(
+                seedForm.getId(),
+                seedForm.getSelectType(),
+                seedForm.getTitle(),
+                seedForm.getContent(),
+                LocalDateTime.now());
+        seedService.updateSeed(updateSeedDTO);
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/targetcoders/sowing/seed/SeedForm.java
+++ src/main/java/com/targetcoders/sowing/seed/SeedForm.java
@@ -3,11 +3,15 @@ package com.targetcoders.sowing.seed;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter @Setter
 public class SeedForm {
 
+    private Long id;
     private String username;
     private String title;
+    private List<SeedType> typeList;
     private SeedType selectType;
     private String content;
 
